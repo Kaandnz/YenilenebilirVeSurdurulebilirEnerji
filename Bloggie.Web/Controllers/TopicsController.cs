36777fadf8488c10c0e5074dc471074832e5c4db
Kaandnz <konmdeniz@gmail.com>
2024-12-01 23:55:38 +0300

    Add project files.

---
+++ Bloggie.Web/Controllers/TopicsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+
+namespace Bloggie.Web.Controllers
+{
+    public class TopicsController : Controller
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public TopicsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+
+        public async Task<IActionResult> WaterEnergy()
+        {
+            var blogPosts = await blogPostRepository.GetByTagAsync("Water Energy");
+            return View(blogPosts);
+        }
+
+
+        public async Task<IActionResult> WindEnergy()
+        {
+            var blogPosts = await blogPostRepository.GetByTagAsync("Wind Energy");
+            return View(blogPosts);
+        }
+
+        public async Task<IActionResult> SolarEnergy()
+        {
+            var blogPosts = await blogPostRepository.GetByTagAsync("Solar Energy");
+            return View(blogPosts);
+        }
+
+        public async Task<IActionResult> GeothermalEnergy()
+        {
+            var blogPosts = await blogPostRepository.GetByTagAsync("Geothermal Energy");
+            return View(blogPosts);
+        }
+
+        public async Task<IActionResult> BiomassEnergy()
+        {
+            var blogPosts = await blogPostRepository.GetByTagAsync("Biomass Energy");
+            return View(blogPosts);
+        }
+
+        public async Task<IActionResult> FutureEnergy()
+        {
+            var blogPosts = await blogPostRepository.GetByTagAsync("Future Energy");
+            return View(blogPosts);
+        }
+    }
+}
---
+++ Bloggie.Web/Models/ViewModels/TopicViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+{
+    public class TopicViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
